ab3dbe5dd4a84e8b485fe3a1b753435b6665dbef
Jaakko Astikainen <jaakko.astikainen@gmail.com>
2024-05-22 13:31:11 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,66 @@
+// Embedded System Project
+// Jaakko Astikainen & Mohamed Amer
+// 2024
+
+// Some standard libraries
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <time.h>
+
+// Include I2S driver
+#include <driver/i2s.h>
+
+// Connections to INMP441 I2S microphone
+#define I2S_WS 25
+#define I2S_SD 33
+#define I2S_SCK 32
+ 
+// Use I2S Processor 0
+#define I2S_PORT I2S_NUM_0
+
+// main finite state machine
+enum state = {
+    START
+}
+
+void *SetupMicTask(void *data)
+{
+    // Set up I2S Processor configuration
+  const i2s_config_t i2s_config = {
+    .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX),
+    .sample_rate = 44100,
+    .bits_per_sample = i2s_bits_per_sample_t(16),
+    .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+    .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_STAND_I2S),
+    .intr_alloc_flags = 0,
+    .dma_buf_count = 8,
+    .dma_buf_len = bufferLen,
+    .use_apll = false
+  };
+ 
+  i2s_driver_install(I2S_PORT, &i2s_config, 0, NULL);
+
+    // Set I2S pin configuration
+  const i2s_pin_config_t pin_config = {
+    .bck_io_num = I2S_SCK,
+    .ws_io_num = I2S_WS,
+    .data_out_num = -1,
+    .data_in_num = I2S_SD
+  };
+ 
+  i2s_set_pin(I2S_PORT, &pin_config);
+}
 
 int main(void)
 {
-    printf("Project template\n");
+    // set up tasks
+    pthread_t SetupMicTask_p;
+
+    pthread_create (&SetupMicTask_p, NULL, SetupMicTask, NULL);
+
+    pthread_join(SetupMicTask_p,NULL);
+    
     return 0;
 }
